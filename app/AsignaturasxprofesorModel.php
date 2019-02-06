e09c6fc50f9472c3ef807b158dfe135b5c9afe17
gyanez02 <32914893+gyanez02@users.noreply.github.com>
2019-02-06 12:28:11 -0500

    modelos

---
+++ app/AsignaturasxprofesorModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AsignaturasxprofesorModel extends Model
+{
+    protected $table = 'profesores';
+	public $timestamps = false;
+}
