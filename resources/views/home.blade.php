c40ad294673eb2c0c51f3ac4035273f92fb8c294
unknown <josesalgado7@hotmail.com>
2019-02-18 21:53:09 -0500

    Jose Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,8 +12,7 @@
             <div class="col-sm-6">
             <select name="codperiodo" id="periodo" data-dependent="periodo" class="form-control input-lg periodo principal" type="text">
                 <option value="">Seleccione el periodo</option>
-                @foreach($periodos as $periodo)
-                    
+                @foreach($periodos as $periodo)                
                     <option value="{{$periodo->codperiodo}}">{{$periodo->codperiodo}}</option>
                 @endforeach
             </select>
@@ -23,9 +22,8 @@
     <div class="form-group">
             <label class="control-label col-sm-3" for="asignatura"></label>
             <div class="col-sm-6">
-                <select class="form-control input-lg dynamic principal" data-dependent="asignatura" id="asignatura" name="asignatura">
-                  <option value="0" disable="true" selected="true">Seleccione la asignatura</option>
-                    
+                <select class="form-control input-lg dynamic principal" onChange="myNewFunction(this);" data-dependent="asignatura" id="asignatura" name="asignatura">
+                  <option value="0" disable="true" selected="true">Seleccione la asignatura</option>                    
                 </select>
             </div>
         </div>
@@ -39,6 +37,7 @@
     $('#periodo').on('change',function(e){
       console.log(e);
       var codperiodo = e.target.value;
+      console.log(codperiodo);
       $.get('/json-asignaturas?codperiodo=' + codperiodo,function(data){
         console.log(data);
         $('#asignatura').empty();
@@ -50,20 +49,71 @@
     });
   
     </script>
+
 @endsection
 
 @section('content')
 <div style="width:100%;">
 <div id="carouselExampleFade" style="width:100%;" class="carousel slide carousel-fade" data-interval="false">
-    <div class="carousel-inner" style="width:100%;" >
+    <div class="carousel-inner" style="width:100%;" role="listbox" >
       <div class="carousel-item active" style="width:100%;">
         <!--<img class="imagen1" src="https://mdbootstrap.com/img/Photos/Slides/img%20(15).jpg" alt="First slide">-->
         <!--<textarea class="imagen1" alt="First slide"></textarea>-->
         <!--<p class="imagen1">dasda</p>-->
         <!--<img class="d-block w-100" src="{{ url('img/beagle.jpg') }}" alt="First slide">-->
-        <div class="d-none d-md-block">          
-          <textarea class="form-controlCanvas" alt="First slide"></textarea>
+        <div class="editor" id="editor" style="text-align: left; width:100%"  contenteditable="false">select isa dasdas isa </div>
+	<script>
+	$("#editor").on("keydown keyup change click load focus mouseover", function(e){
+    if (e.keyCode == 32){
+        var text = $(this).text().replace(/[\s]+/g, " ").trim();
+        var word = text.split(" ");
+        var newHTML = "";
+
+        $.each(word, function(index, value){
+            switch(value.toUpperCase()){
+                case "SELECT":
+                case "FROM":
+                case "WHERE":
+                case "LIKE":
+                case "BETWEEN":
+                case "NOTLIKE":
+                case "FALSE":
+                case "NULL":
+                case "ISA":
+                case "TRUE":
+                case "PATITO":
+				case "NOTIN":
+                    newHTML += "<span class='statement'>" + value + "&nbsp;</span>";
+                    break;
+                default: 
+                    newHTML += "<span class='other'>" + value + "&nbsp;</span>";
+            }
+        });
+      	$(this).html(newHTML);
+        
+        //// Set cursor postion to end of text
+        var child = $(this).children();
+        var range = document.createRange();
+        var sel = window.getSelection();
+        range.setStart(child[child.length - 1], 1);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+        $(this)[0].focus(); 
+    }
+});
+	</script>
+       <!-- <div class="d-none d-md-block">          
+          <textarea id="status" class="form-controlCanvas" alt="First slide">hola ke ase</textarea>
         </div>
+        
+
+        <script type="text/javascript">
+        function myNewFunction(sel) {
+          var textF= sel.options[sel.selectedIndex].text;
+          alert(textF);
+        }
+        </script>-->
       </div>
       <div class="carousel-item">
         <img class="imagen1" src="https://mdbootstrap.com/img/Photos/Slides/img%20(16).jpg" alt="Second slide">
@@ -92,7 +142,7 @@
         <div class="colorHeaderToggle" style="font-size:14px">
             <label for="comment">Anotaciones: </label><br>
             <label class="labelC" style="font-size:9px">Caracteres restantes: <span style="color: white;">500</span></label>
-            <textarea maxlength="500" class="form-control" style="margin-bottom: 1rem;" name="anotaciones" id="comentarioEstudiante" rows="19" autofocus></textarea>
+            <textarea maxlength="500" class="form-control altoAnotaciones" style="margin-bottom: 1rem;" name="anotaciones" id="comentarioEstudiante" rows="19" autofocus></textarea>
         </div>
       </td>
     <tr>
